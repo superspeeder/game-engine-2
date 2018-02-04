43d4d89496d89196596a4ff4c92176aa2a680e63
superspeeder101 <andy@delusion.org>
2018-02-03 18:50:29 -0800

    more additions

--- engine/tests/test_color.py
+++ engine/tests/test_color.py
@@ -1,1 +1,26 @@
-import base_utils as util
+from base_utils import Color
+from shortcuts.base import update, make_display, make_group, SpriteBuilder, quit_conditional
+from sprites.base import Square
+
+import pygame
+from pygame.locals import *
+
+BACKGROUND_COLOR = Color(255,255,255)
+RED = Color(255,0,0)
+
+
+screen = make_display((800,800), BACKGROUND_COLOR)
+all_draw = make_group()
+
+square = SpriteBuilder(Square)
+
+
+
+all_draw.add(square((300,300), RED))
+
+update()
+
+
+while 1:
+    for event in pygame.event.get():
+        quit_conditional(event)
